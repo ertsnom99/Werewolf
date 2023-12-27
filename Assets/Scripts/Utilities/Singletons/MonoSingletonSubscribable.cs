254bd6dc0e079ff4ca9ba8256866997635cd6c25
Ertsnom99 <Xbox99972@hotmail.com>
2023-12-27 06:40:05 -0500

    Added simple scriptable objects for each role

---
+++ Assets/Scripts/Utilities/Singletons/MonoSingletonSubscribable.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class MonoSingletonSubscribable<T, U> : MonoSingleton<U> where U : MonoBehaviour
+{
+    protected List<T> Subscribers = new List<T>();
+
+    public virtual void Subscribe(T subscriber)
+    {
+        Subscribers.Add(subscriber);
+    }
+
+    public virtual void Unsubscribe(T subscriber)
+    {
+        Subscribers.Remove(subscriber);
+    }
+}
