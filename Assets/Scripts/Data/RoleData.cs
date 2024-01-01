782e4a2e8a884bd95edca01515f08f0853d16f3c
Ertsnom99 <Xbox99972@hotmail.com>
2023-12-31 21:44:50 -0500

    Added RolesSetupData

--- Assets/Scripts/Data/RoleData.cs
+++ Assets/Scripts/Data/RoleData.cs
@@ -1,7 +1,6 @@
-using System;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Role", menuName = "ScriptableObjects/Role")]
+[CreateAssetMenu(fileName = "Role", menuName = "ScriptableObjects/Roles/Role")]
 public class RoleData : ScriptableObject
 {
     [field: SerializeField]
@@ -18,6 +17,14 @@ public class RoleData : ScriptableObject
     [field: SerializeField]
     [field: PreviewSprite]
     public Sprite Image { get; private set; }
+
+    // This allows to have more than once this role in a game
+    [field: SerializeField]
+    public bool CanHaveMultiples { get; private set; }
+
+    // When CanHaveMultiples is false, there will be this exact number of this role at once in a game 
+    [field: SerializeField]
+    public int MandatoryCount { get; private set; }
 
     [field: SerializeField]
     public int[] GroupIndexes { get; private set; }
