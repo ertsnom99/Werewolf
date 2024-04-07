9c98758595c6784c1aa6bc4ac7ef13c897ae0eed
Ertsnom99 <Xbox99972@hotmail.com>
2024-04-06 23:12:35 -0400

    Refactored the end game code to reduce the number of RPC calls to only 1

--- Assets/Scripts/Data/PlayerGroupsData.cs
+++ Assets/Scripts/Data/PlayerGroupsData.cs
@@ -20,6 +20,9 @@ namespace Werewolf.Data
 		[field: SerializeField]
 		public PlayerGroupData[] Datas { get; private set; }
 
+		[field: SerializeField]
+		public int NoBehaviorGroupIndex { get; private set; }
+
 		public PlayerGroupData GetPlayerGroupData(int index)
 		{
 			foreach(PlayerGroupData data in Datas)
