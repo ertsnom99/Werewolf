215c34b0d1013a163c6603e92fe4617141e6d74d
Ertsnom99 <Xbox99972@hotmail.com>
2024-06-16 00:21:58 -0400

    Added roles in main menu

--- Assets/Scripts/Utilities/GameplayTags/Managers/GameplayDatabaseManager.cs
+++ Assets/Scripts/Utilities/GameplayTags/Managers/GameplayDatabaseManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public class GameplayDatabaseManager : MonoSingleton<GameplayDatabaseManager>
+public class GameplayDatabaseManager : KeptMonoSingleton<GameplayDatabaseManager>
 {
 	[SerializeField]
 	private string[] _foldersToLoad;
