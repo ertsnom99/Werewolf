042be38de209a1a04964859897efbe836459e6e6
Ertsnom99 <Xbox99972@hotmail.com>
2024-06-29 04:18:22 -0400

    Added the localized texts for the game history

---
+++ Assets/Localization/CustomScripts/LocalizedStringList.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.Localization.SmartFormat.Core.Extensions;
+
+namespace UnityEngine.Localization.SmartFormat.PersistentVariables
+{
+	// From: https://forum.unity.com/threads/using-list-formatter-for-an-array-of-localizedstring.1394401/
+	[Serializable]
+	public class LocalizedStringList : IVariable
+	{
+		public List<LocalizedString> Values = new();
+
+		public object GetSourceValue(ISelectorInfo selector)
+		{
+			return Values.Select(l => l.GetLocalizedString()).ToList();
+		}
+	}
+}
