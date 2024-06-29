a538ada0596d805ea69fc8b30e06722efa0fcc96
Ertsnom99 <Xbox99972@hotmail.com>
2024-06-29 04:18:22 -0400

    Added the localized texts for the game history

---
+++ Assets/Localization/CustomScripts/StringList.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine.Localization.SmartFormat.Core.Extensions;
+
+namespace UnityEngine.Localization.SmartFormat.PersistentVariables
+{
+	[Serializable]
+	public class StringList : IVariable
+	{
+		public List<string> Values = new();
+
+		public object GetSourceValue(ISelectorInfo selector)
+		{
+			return Values;
+		}
+	}
+}
