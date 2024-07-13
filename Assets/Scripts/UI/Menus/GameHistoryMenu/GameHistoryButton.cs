ed3d5e24be909562a7be4bd8a526a686ac21e560
Ertsnom99 <Xbox99972@hotmail.com>
2024-07-13 19:41:00 -0400

    Highlighted selected game history button

--- Assets/Scripts/UI/Menus/GameHistoryMenu/GameHistoryButton.cs
+++ Assets/Scripts/UI/Menus/GameHistoryMenu/GameHistoryButton.cs
@@ -2,27 +2,55 @@ using System;
 using System.IO;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Werewolf.UI
 {
+	[RequireComponent(typeof(Image))]
 	public class GameHistoryButton : MonoBehaviour
 	{
+		[Header("Text")]
 		[SerializeField]
 		private TextMeshProUGUI _name;
+		[SerializeField]
+		private Color _normalNameColor;
+		[SerializeField]
+		private Color _selectedNameColor;
 
-		private string _filePath;
+		[Header("Background")]
+		[SerializeField]
+		private Color _oddColor;
+		[SerializeField]
+		private Color _evenColor;
 
-		public event Action<string> Clicked;
+		public string FilePath { get; private set; }
 
-		public void Initialize(string filePath)
+		private Image _background;
+
+		public event Action<GameHistoryButton> Clicked;
+
+		private void Awake()
 		{
-			_filePath = filePath;
+			_background = GetComponent<Image>();
+		}
+
+		public void Initialize(string filePath, bool isOdd)
+		{
+			FilePath = filePath;
 			_name.text = Path.GetFileNameWithoutExtension(filePath);
+
+			_background.color = isOdd ? _oddColor : _evenColor;
 		}
 
 		public void OnClicked()
 		{
-			Clicked?.Invoke(_filePath);
+			SetSelected(true);
+			Clicked?.Invoke(this);
+		}
+
+		public void SetSelected(bool selected)
+		{
+			_name.color = selected? _selectedNameColor : _normalNameColor;
 		}
 	}
 }
