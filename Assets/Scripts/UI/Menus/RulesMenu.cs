da8712854b79db770f9d692c24dc357280fd6984
Ertsnom99 <Xbox99972@hotmail.com>
2024-12-15 19:13:19 -0500

    Added emotes rules

--- Assets/Scripts/UI/Menus/RulesMenu.cs
+++ Assets/Scripts/UI/Menus/RulesMenu.cs
@@ -11,18 +11,30 @@ namespace Werewolf.UI
 		[SerializeField]
 		private GameObject _roles;
 
+		[SerializeField]
+		private GameObject _emotes;
+
 		public event Action ReturnClicked;
 
 		public void OnShowRules()
 		{
 			_rules.SetActive(true);
 			_roles.SetActive(false);
+			_emotes.SetActive(false);
 		}
 
 		public void OnShowRoles()
 		{
 			_rules.SetActive(false);
 			_roles.SetActive(true);
+			_emotes.SetActive(false);
+		}
+
+		public void OnShowEmotes()
+		{
+			_rules.SetActive(false);
+			_roles.SetActive(false);
+			_emotes.SetActive(true);
 		}
 
 		public void OnReturn()
